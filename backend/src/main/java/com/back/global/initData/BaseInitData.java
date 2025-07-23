b5b57a2d1bb0bb873209e7a591a62d21b3cd6e55
Jiwoo Seo <sjwoos123@gmail.com>
2025-07-23 14:38:45 +0900

    setting: BE 기본 환경 세팅2

---
+++ backend/src/main/java/com/back/global/initData/BaseInitData.java
@@ -0,0 +1,12 @@
+package com.back.global.initData;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class BaseInitData {
+
+}
