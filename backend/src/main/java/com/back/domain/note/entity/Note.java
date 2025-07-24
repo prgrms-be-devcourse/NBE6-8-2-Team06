97cb46c48a50b4828ef3c0718d489318fe4de3b9
ohsoohyuk <okzzangsh@gmail.com>
2025-07-24 16:01:59 +0900

    feat: 노트 생성, 수정, 삭제 구현

--- backend/src/main/java/com/back/domain/note/entity/Note.java
+++ backend/src/main/java/com/back/domain/note/entity/Note.java
@@ -4,15 +4,24 @@ import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Note extends BaseEntity {
     private String title;
     private String content;
 
-//    // 연관관계 매핑(엔티티 병합 전)
+//     bookmark 병합해야함
 //    @ManyToOne
+//    @JoinColumn(name = "bookmark_id")
 //    private Bookmark bookmark;
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+//        this.bookmark = bookmark;
+    }
 }
