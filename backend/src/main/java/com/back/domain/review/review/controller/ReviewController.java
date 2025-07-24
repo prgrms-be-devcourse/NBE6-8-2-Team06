9fdf6fcb48cd122fcd5eb7e705c992d418059eea
ehrl1225 <wjdwntls1225@naver.com>
2025-07-24 14:31:56 +0900

    [#13]fix: 잘못된 메소드 수정

--- backend/src/main/java/com/back/domain/review/review/controller/ReviewController.java
+++ backend/src/main/java/com/back/domain/review/review/controller/ReviewController.java
@@ -2,20 +2,16 @@ package com.back.domain.review.review.controller;
 
 import com.back.domain.book.book.entity.Book;
 import com.back.domain.book.book.service.BookService;
-import com.back.domain.member.member.entity.Member;
+
+import com.back.domain.member.member.service.MemberService;
 import com.back.domain.review.review.dto.ReviewRequestDto;
-import com.back.domain.review.review.entity.Review;
 import com.back.domain.review.review.service.ReviewService;
+import com.back.domain.member.member.entity.Member;
 import com.back.global.exception.ServiceException;
 import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
@@ -25,11 +21,11 @@ public class ReviewController {
     private final BookService bookService;
     private final Rq rq;
 
-    @RequestMapping("/{book_id}")
-    public RsData<Void> getReviews(@PathVariable("book_id") int bookId, @RequestBody ReviewRequestDto reviewRequestDto) {
+    @PostMapping("/{book_id}")
+    public RsData<Void> create(@PathVariable("book_id") int bookId, @RequestBody ReviewRequestDto reviewRequestDto) {
         Book book = bookService.findById(bookId).orElseThrow(()->new ServiceException("404-1", "Book not found"));
         Member member = rq.getActor();
         reviewService.addReview(book, member, reviewRequestDto);
-        return new RsData<>("200-1", "Reviews fetched successfully");
+        return new RsData<>("201-1", "Reviews fetched successfully");
     }
 }
