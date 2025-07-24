1caf53c5a8ec049c70759d66e1c932948583f447
osh5030 <72571931+ohsoohyuk@users.noreply.github.com>
2025-07-24 09:15:29 +0900

    Feat: 내 노트 다건, 단건조회 (#29)

---
+++ backend/src/main/java/com/back/domain/note/dto/NoteDto.java
@@ -0,0 +1,24 @@
+package com.back.domain.note.dto;
+
+import com.back.domain.note.entity.Note;
+import org.springframework.lang.NonNull;
+
+import java.time.LocalDateTime;
+
+public record NoteDto(
+        @NonNull int id,
+        @NonNull LocalDateTime createDate,
+        @NonNull LocalDateTime modifyDate,
+        @NonNull String title,
+        @NonNull String content
+) {
+    public NoteDto(Note note) {
+        this(
+                note.getId(),
+                note.getCreateDate(),
+                note.getModifyDate(),
+                note.getTitle(),
+                note.getContent()
+        );
+    }
+}
