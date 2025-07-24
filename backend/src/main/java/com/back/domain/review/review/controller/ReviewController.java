f857ce948cce0f4b0eaec4a302da57ea642134e2
ehrl1225 <ehrl1225@github.com>
2025-07-24 10:13:17 +0900

    [#13]feat: 추가된 기능 적용

--- backend/src/main/java/com/back/domain/review/review/controller/ReviewController.java
+++ backend/src/main/java/com/back/domain/review/review/controller/ReviewController.java
@@ -6,17 +6,18 @@ import com.back.domain.member.member.entity.Member;
 import com.back.domain.review.review.dto.ReviewRequestDto;
 import com.back.domain.review.review.entity.Review;
 import com.back.domain.review.review.service.ReviewService;
+import com.back.global.exception.ServiceException;
 import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
@@ -26,7 +27,7 @@ public class ReviewController {
 
     @RequestMapping("/{book_id}")
     public RsData<Void> getReviews(@PathVariable("book_id") int bookId, @RequestBody ReviewRequestDto reviewRequestDto) {
-        Book book = bookService.findById(bookId).orElseThrow(RuntimeException::new); // TODO need GlobalExceptionHandler and ServiceException
+        Book book = bookService.findById(bookId).orElseThrow(()->new ServiceException("404-1", "Book not found"));
         Member member = rq.getActor();
         reviewService.addReview(book, member, reviewRequestDto);
         return new RsData<>("200-1", "Reviews fetched successfully");
