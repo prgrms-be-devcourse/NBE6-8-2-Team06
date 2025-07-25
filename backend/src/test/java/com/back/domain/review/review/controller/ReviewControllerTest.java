139804ef4dfc98420a169b13512498aa2cac0670
ehrl1225 <wjdwntls1225@naver.com>
2025-07-25 10:24:41 +0900

    [#36]feat: 리뷰 수정 기능 테스트

--- backend/src/test/java/com/back/domain/review/review/controller/ReviewControllerTest.java
+++ backend/src/test/java/com/back/domain/review/review/controller/ReviewControllerTest.java
@@ -16,8 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -62,7 +61,7 @@ public class ReviewControllerTest {
                 .andExpect(jsonPath("$.msg").value("Reviews fetched successfully"))
                 ;
 
-        assertThat(review.getId()).isEqualTo(1);
+        assertThat(review.getId()).isGreaterThan(0);
     }
 
     @Test
@@ -81,7 +80,7 @@ public class ReviewControllerTest {
 """).cookie(new Cookie("accessToken", accessToken))
         ).andDo(print());
         Review review = reviewService.findLatest().orElseThrow(()-> new RuntimeException("리뷰가 없습니다."));
-        assertThat(review.getId()).isEqualTo(1);
+        assertThat(review.getId()).isGreaterThan(0);
 
         ResultActions resultActions = mvc.perform(
                 delete("/reviews/{book_id}", 1)
@@ -97,6 +96,47 @@ public class ReviewControllerTest {
                 .andExpect(jsonPath("$.msg").value("Review deleted successfully"))
         ;
         assertThat(reviewService.findLatest()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("리뷰 수정")
+    void t3() throws Exception {
+        Member member = memberService.findByEmail("email").get();
+        String accessToken = authTokenService.genAccessToken(member);
+        mvc.perform(
+                post("/reviews/{book_id}", 1)
+                        .contentType("application/json")
+                        .content("""
+{
+    "content": "이 책 정말 좋았어요!",
+    "rate": 5
+}
+""").cookie(new Cookie("accessToken", accessToken))
+        ).andDo(print());
+        Review review = reviewService.findLatest().orElseThrow(()-> new RuntimeException("리뷰가 없습니다."));
+        assertThat(review.getId()).isGreaterThan(0);
+        ResultActions resultActions = mvc.perform(
+                put("/reviews/{book_id}", 1)
+                        .contentType("application/json")
+                        .content("""
+{
+    "content": "다시 읽다보니 그렇게 좋지는 않네요.",
+    "rate": 4
+}
+""").cookie(new Cookie("accessToken", accessToken))
+        ).andDo(print());
+        resultActions
+                .andExpect(handler().handlerType(ReviewController.class))
+                .andExpect(handler().methodName("modify"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("Review modified successfully"))
+        ;
+        review = reviewService.findLatest().orElseThrow(() -> new RuntimeException("리뷰가 없습니다."));
+        assertThat(review.getId()).isGreaterThan(0);
+        assertThat(review.getContent()).isEqualTo("다시 읽다보니 그렇게 좋지는 않네요.");
+        assertThat(review.getRate()).isEqualTo(4);
+
 
     }
 
