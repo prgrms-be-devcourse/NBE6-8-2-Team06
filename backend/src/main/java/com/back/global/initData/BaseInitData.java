9bf671278a55c1edff0f0edbf67209a52d843b27
ehrl1225 <wjdwntls1225@naver.com>
2025-07-25 09:53:06 +0900

    [#35]feat: 리뷰 테스트 중 필요한 초기 데이터

--- backend/src/main/java/com/back/global/initData/BaseInitData.java
+++ backend/src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,6 +1,11 @@
 package com.back.global.initData;
 
 
+import com.back.domain.book.book.entity.Book;
+import com.back.domain.book.book.repository.BookRepository;
+import com.back.domain.book.category.entity.Category;
+import com.back.domain.book.category.repository.CategoryRepository;
+import com.back.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -16,16 +21,22 @@ public class BaseInitData {
     @Autowired
     @Lazy
     private BaseInitData self;
+    private final MemberService memberService;
+    private final CategoryRepository categoryRepository;
+    private final BookRepository bookRepository;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner(){
         return args->{
-
+//            self.initReviewData(); // 리뷰 테스트 시 주석 해제
         };
     }
 
     @Transactional
     public void initReviewData() {
+        memberService.join("testUser", "email", "password");
+        Category category = categoryRepository.save(new Category("Test Category"));
+        bookRepository.save(new Book("Text Book", "Publisher", category));
 
     }
 }
