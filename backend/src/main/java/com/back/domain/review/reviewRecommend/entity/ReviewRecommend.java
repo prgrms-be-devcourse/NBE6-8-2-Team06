3d53a76bdf5bdac490f5b3df7b523b1f62784e99
ehrl1225 <wjdwntls1225@naver.com>
2025-07-25 14:43:51 +0900

    [#48]feat: 엔티티 구현

--- backend/src/main/java/com/back/domain/review/reviewRecommend/entity/ReviewRecommend.java
+++ backend/src/main/java/com/back/domain/review/reviewRecommend/entity/ReviewRecommend.java
@@ -1,9 +1,25 @@
 package com.back.domain.review.reviewRecommend.entity;
 
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.review.review.entity.Review;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
 public class ReviewRecommend extends BaseEntity {
+    boolean isRecommended;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Review review;
 
+    public ReviewRecommend(Review review, Member member, boolean isRecommended) {
+        this.review = review;
+        this.member = member;
+        this.isRecommended = isRecommended;
+    }
 }
