2a376014c20c2810bac6073c1809c7248473dc76
ehrl1225 <wjdwntls1225@naver.com>
2025-07-25 14:44:51 +0900

    [#48]feat: 기능 구현

--- backend/src/main/java/com/back/domain/review/reviewRecommend/controller/ReviewRecommendController.java
+++ backend/src/main/java/com/back/domain/review/reviewRecommend/controller/ReviewRecommendController.java
@@ -1,7 +1,42 @@
 package com.back.domain.review.reviewRecommend.controller;
 
+import com.back.domain.book.book.entity.Book;
+import com.back.domain.book.book.repository.BookRepository;
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.review.review.entity.Review;
+import com.back.domain.review.review.service.ReviewService;
+import com.back.domain.review.reviewRecommend.service.ReviewRecommendService;
+import com.back.global.exception.ServiceException;
+import com.back.global.rq.Rq;
+import com.back.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/reviews/{book_id}/recommend")
+@RequiredArgsConstructor
 public class ReviewRecommendController {
+    private final Rq rq;
+    private final BookRepository bookRepository;
+    private final ReviewService reviewService;
+    private final ReviewRecommendService reviewRecommendService;
+
+
+    @PostMapping("{isRecommend}")
+    public RsData<Void> recommendReview(@PathVariable("book_id") int bookId, @PathVariable("isRecommend") boolean isRecommend) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new ServiceException("404-1", "Book not found"));
+        Member member = rq.getActor();
+        if (member == null) {
+            return new RsData<>("401-1", "Unauthorized access");
+        }
+        Review review = reviewService.findByBookAndMember(book, member)
+                .orElseThrow(() -> new ServiceException("404-1", "Review not found"));
+        reviewRecommendService.recommendReview(review, member, isRecommend);
+        return new RsData<>("200-1", "Review recommended successfully");
+    }
+
 }
--- backend/src/main/java/com/back/domain/review/reviewRecommend/service/ReviewRecommendService.java
+++ backend/src/main/java/com/back/domain/review/reviewRecommend/service/ReviewRecommendService.java
@@ -1,7 +1,29 @@
 package com.back.domain.review.reviewRecommend.service;
 
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.review.review.entity.Review;
+import com.back.domain.review.review.service.ReviewService;
+import com.back.domain.review.reviewRecommend.entity.ReviewRecommend;
+import com.back.domain.review.reviewRecommend.repository.ReviewRecommendRepository;
+import com.back.global.exception.ServiceException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class ReviewRecommendService {
+    private final ReviewRecommendRepository reviewRecommendRepository;
+    private final ReviewService reviewService;
+
+    @Transactional
+    public void recommendReview(Review review, Member member, boolean isRecommend) {
+        ReviewRecommend reviewRecommend = new ReviewRecommend(review, member, isRecommend);
+        if (reviewRecommendRepository.findByReviewAndMember(review, member).isPresent()) {
+            throw new ServiceException("400-1", "Review recommendation already exists");
+        }
+        review.getReviewRecommends().add(reviewRecommend);
+        review.setRecommendCount(review.getReviewRecommends().size());
+        reviewRecommendRepository.save(reviewRecommend);
+    }
 }
