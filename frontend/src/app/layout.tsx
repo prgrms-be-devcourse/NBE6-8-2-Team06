d700f691b4a1be7cd1b6d36711e84693457a0d42
ehrl1225 <wjdwntls1225@naver.com>
2025-07-31 15:11:38 +0900

    [#125]feat: 화이트 모드 <-> 다크 모드 버튼 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -24,7 +24,10 @@ const metadata: Metadata = {
 import Link from "next/link";
 
 import React from "react";
-import { BookOpen, Home, User, LogOut, Library } from "lucide-react";
+import { BookOpen, Home, User, LogOut, Library, Sun, Moon } from "lucide-react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes";
 
 import { Toaster } from "@/components/ui/sonner";
 type Page = "home" | "login" | "books" | "bookmark" | "profile";
@@ -34,6 +37,32 @@ interface NavigationProps {
   onNavigate: (page: Page) => void;
   isLoggedIn: boolean;
   onLogout: () => void;
+}
+
+function ThemeButton(){
+  const { setTheme } = useTheme()
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
 
 function NavigationContent({ children }: { children: React.ReactNode }) {
@@ -115,6 +144,7 @@ function NavigationContent({ children }: { children: React.ReactNode }) {
                   로그인
                 </Button>
               )}
+              <ThemeButton />
             </div>
           </div>
         </div>
@@ -130,13 +160,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          <NavigationContent>{children}</NavigationContent>
-        </AuthProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthProvider>
+            <NavigationContent>{children}</NavigationContent>
+            
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
