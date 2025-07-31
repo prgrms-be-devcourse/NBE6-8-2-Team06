7debdc270dde0f4a6ed14c946114020eaf98efa7
ehrl1225 <wjdwntls1225@naver.com>
2025-07-31 16:30:56 +0900

    [#125]refactor: defaultTheme is light

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -166,7 +166,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
